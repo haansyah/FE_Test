1d9c1cb7b191a7021bf0649b9ac5754c6bbb6070
haansyah <hansyah242@gmail.com>
2025-02-03 10:20:26 +0700

    fix(/ and dashboard): update ui

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,9 +1,75 @@
 "use client";
 
 import Map from "@/components/custom/Map";
-import React from "react";
+import api from "@/lib/axios";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
 
 export default function Page() {
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState([]);
+  const [coordinates, setCoordinates] = React.useState([]);
+  const [params, setParams] = React.useState({
+    show: "active_only",
+  });
+
+  const fecthData = async () => {
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
+      await api
+        .get("/ruas", {
+          params,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          setCoordinates(
+            res?.data
+              ?.map((item: any) =>
+                item.coordinates.map((coord: any) => coord.coordinates)
+              )
+              .flat()
+          );
+        })
+        .then(async () => {
+          const res = await api.post(
+            "/ruas/generate-routes",
+            { coordinates },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setData(res.data);
+          console.log("check", res);
+        })
+        .catch((err) => {
+          if (err.status === 401) {
+            toast.error("Unauthorized");
+            localStorage.removeItem("token");
+            window.location.href = "/auth/login";
+          }
+        })
+        .finally(() => {
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        });
+    } catch (error) {
+      toast.error("Failed to fetch data");
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
   return (
     <div className="overflow-hidden max-w-7xl h-full">{/* <Map /> */}</div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import api from "@/lib/axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+
+  const token = localStorage.getItem("token");
+
+  const [isValidToken, setIsValidToken] = useState(false);
+
+  useEffect(() => {
+    const validateToken = async () => {
+      if (token) {
+        setLoading(true);
+        try {
+          const response = await api.get("/user", {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (response.status === 200) {
+            setIsValidToken(true);
+          } else {
+            localStorage.removeItem("token");
+          }
+        } catch (error) {
+          localStorage.removeItem("token");
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    validateToken();
+  }, [token]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isValidToken) {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to the home page</p>
+        <a href="/dashboard">
+          <Button asChild>
+            <Link href="/admin/dashboard">Go to dashboard</Link>
+          </Button>
+        </a>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to the home page</p>
+      <a href="/login">
+        <Button asChild>
+          <Link href="/auth/login">Login</Link>
+        </Button>
+      </a>
     </div>
   );
 }
